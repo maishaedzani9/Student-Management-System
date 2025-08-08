6df42f2a8499d1e5eba864a6aa2b03a7586f9f64
EDZANI <114071296+maishaedzani9@users.noreply.github.com>
2025-08-08 13:17:40 +0200

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,65 @@
+public class Person {
+    // Private fields for encapsulation
+    private String id;
+    private String name;
+    private String email;
+
+    // Default constructor
+    public Person() {}
+
+    // Parameterized constructor
+    public Person(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    // Equals and hashCode for proper object comparison
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id.equals(person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
